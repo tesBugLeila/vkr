ef2c718962fbbfe6b5d590879fe88a1bdce37ee7
Pavel Denisov <pv.denisov@interfax.ru>
2026-01-12 18:23:43 +0700

    Эммитация авторизации

---
+++ local-food-board-front/src/app/components/loading/loading.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  imports: [],
+  standalone: true,
+  templateUrl: './loading.html',
+  styleUrl: './loading.scss',
+})
+export class Loading {
+
+}
--- local-food-board-front/src/app/components/login-bar/login-bar.ts
+++ local-food-board-front/src/app/components/login-bar/login-bar.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../services/user';
+import { NgxMaskPipe } from 'ngx-mask';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-bar',
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink, NgxMaskPipe],
   templateUrl: './login-bar.html',
   styleUrl: './login-bar.scss',
   standalone: true,
 })
 export class LoginBar {
+  constructor(
+    public userService: UserService,
+    private router: Router,
+  ) {}
 
+  logOut() {
+    this.userService.userPhoneNumber = null;
+    this.router.navigate(['/']).then();
+  }
 }
