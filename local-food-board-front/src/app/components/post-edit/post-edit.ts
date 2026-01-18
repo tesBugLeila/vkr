b55f25939499e51e2ac59113e69f39e5c96e9278
Pavel Denisov <pv.denisov@interfax.ru>
2026-01-18 20:19:33 +0700

    edit form proto

--- local-food-board-front/src/app/components/post-edit/post-edit.ts
+++ local-food-board-front/src/app/components/post-edit/post-edit.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './post-edit.html',
   styleUrl: './post-edit.scss',
   standalone: true,
 })
-export class PostEdit {
+export class PostEdit implements OnInit {
+  postForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      title: [''],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      category: ['', Validators.required],
+      district: [''],
+      photos: [''],
+      lat: [],
+      lon: [],
+      notifyNeighbors: [true, Validators.required],
+    });
+  }
 }
