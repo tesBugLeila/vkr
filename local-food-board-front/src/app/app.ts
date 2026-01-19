20f1b20a9963e359bdd72b781d9b4bc192980aac
Denisov Pavel <pv.denisov@interfax.ru>
2026-01-20 03:24:39 +0700

    Прототип редактора поста

--- local-food-board-front/src/app/app.ts
+++ local-food-board-front/src/app/app.ts
@@ -3,16 +3,17 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { SearchBar } from './components/search-bar/search-bar';
 import { LoginBar } from './components/login-bar/login-bar';
 import { UserService } from './services/user';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [SearchBar, RouterOutlet, RouterLink, LoginBar],
+  imports: [SearchBar, RouterOutlet, RouterLink, LoginBar, AsyncPipe],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(public userService: UserService) {}
 
  ngOnInit() {
     this.userService.me()
--- local-food-board-front/src/app/components/post-edit/post-edit.ts
+++ local-food-board-front/src/app/components/post-edit/post-edit.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IPost } from '../../types/post';
 
 @Component({
   selector: 'app-post-edit',
@@ -10,19 +11,57 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class PostEdit implements OnInit {
   postForm: FormGroup;
+  @Input() postData?: IPost;
+  editForm!: FormGroup;
+  categories = ['OTHER', 'PIES', 'JAMS', 'VEGETABLES', 'DAIRY', 'MEAT', 'BAKERY'];
+
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.postForm = this.fb.group({
-      title: [''],
-      description: ['', Validators.required],
-      price: ['', Validators.required],
-      category: ['', Validators.required],
+    this.initForm();
+    if (this.postData) {
+      this.editForm.patchValue(this.postData);
+    } else {
+      this.getLocation();
+    }
+  }
+
+  private initForm(): void {
+    this.editForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      price: [0, [Validators.required, Validators.min(0)]],
+      category: ['OTHER', [Validators.required]],
       district: [''],
-      photos: [''],
-      lat: [],
-      lon: [],
-      notifyNeighbors: [true, Validators.required],
+      photos: [[]],
+      lat: [null],
+      lon: [null],
+      notifyNeighbors: [false],
+      // Скрытые поля сохраняем в форме или обрабатываем отдельно при отправке
+      id: [null],
+      contact: [''],
+      userId: [null],
+      createdAt: [new Date()]
     });
+  }
+
+  getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.editForm.patchValue({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        });
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.editForm.valid) {
+      const updatedPost: IPost = this.editForm.value;
+      console.log('Данные для сохранения:', updatedPost);
+      // Здесь вызов сервиса: this.postService.update(updatedPost)
+    }
   }
 }
