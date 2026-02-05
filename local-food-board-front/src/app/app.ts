4c060e162426ab449ace84f321689f94d4936e8c
Denisov Pavel <pv.denisov@interfax.ru>
2026-02-06 02:29:33 +0700

    add footer

--- local-food-board-front/src/app/app.ts
+++ local-food-board-front/src/app/app.ts
@@ -3,17 +3,18 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { SearchBar } from './components/search-bar/search-bar';
 import { LoginBar } from './components/login-bar/login-bar';
 import { UserService } from './services/user.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [SearchBar, RouterOutlet, RouterLink, LoginBar, AsyncPipe],
+  imports: [SearchBar, RouterOutlet, RouterLink, LoginBar, AsyncPipe, DatePipe],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   constructor(public userService: UserService) {}
+  now = new Date();
 
  ngOnInit() {
     this.userService.me()
